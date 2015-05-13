4dcd6dd4e6d6016ab9ad57a15a99b3e52e3593f9
alex <abondar1992@gmail.com>
2015-05-13 18:21:25 +0300

    to Github

---
+++ src/main/java/org/abondar/javaLabs/EventFinder.java
@@ -0,0 +1,229 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.abondar.javaLabs;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.core.Response;
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Attribute;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import org.jsoup.select.Elements;
+
+/**
+ *
+ * @author alex
+ */
+public class EventFinder {
+
+    private Document doc;
+    private final HashMap<String, String> eventTypes;
+
+    public EventFinder() {
+        eventTypes = new HashMap<>();
+        eventTypes.put("theatre", "Teaтры");
+        eventTypes.put("lectures", "Лекции");
+        eventTypes.put("club", "Клубы");
+        eventTypes.put("art", "Выставки");
+        eventTypes.put("concert", "Концерты");
+
+    }
+
+    ;
+    
+    
+
+    public void getCategorizedEvents(String type) {
+        try {
+
+            doc = Jsoup.connect("https://afisha.yandex.ru/msk/events/?category=" + type + "&limit=1000").get();
+
+            Elements els = doc.select("a[href]");
+
+            for (Element e : els) {
+
+                for (Attribute attr : e.attributes().asList()) {
+
+                    if (attr.getValue().contains("clck.yandex.ru")) {
+
+                        if (attr.getValue().charAt(97) != '/') {
+                            getEvent(attr.getValue().substring(90, 96), type);
+
+                        } else {
+                            getEvent(attr.getValue().substring(90, 97), type);
+
+                        }
+                    }
+                }
+
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(EventFinder.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void getEvent(String eventId, String evType) {
+        try {
+
+            Document dc = Jsoup.connect("https://afisha.yandex.ru/msk/events/" + eventId + "/").get();
+
+            EventBean eb = new EventBean();
+            eb.setEventID(eventId);
+            eb.setCategory(eventTypes.get(evType));
+            Elements elems = dc.select("meta");
+
+            for (Element e : elems) {
+                if (e.attributes().get("property").contains("og:description")) {
+                    eb.setDescription(e.attributes().get("content"));
+
+                }
+
+            }
+
+            elems = dc.select("title");
+
+            for (Element e : elems) {
+
+                eb.setName(e.html().substring(0, e.html().indexOf("–")));
+            }
+
+            elems = dc.select("a[href]");
+
+            for (Element e : elems) {
+
+                for (Attribute attr : e.attributes().asList()) {
+
+                    if (attr.getValue().contains("/msk/places/")) {
+
+                        eb.setPlace(getEventPlaces(attr.getValue()));
+
+                    }
+                }
+
+            }
+
+            elems = dc.select("tr[id]");
+
+            for (Element e : elems) {
+                for (Attribute attr : e.attributes().asList()) {
+
+                    if (attr.getValue().contains("f")) {
+
+                        eb.setDate(e.children().first().html());
+
+                        try {
+                            Element e1 = e.child(1).children().first();
+                            Element e2 = e1.children().first();
+                            Element e3 = e2.children().first();
+                            Element e4 = e3.children().first();
+
+                            eb.setTime(e4.html());
+
+                        } catch (NullPointerException ex) {
+
+                            Element e1 = e.child(2).children().first();
+                            Element e2 = e1.children().first();
+                            Element e3 = e2.children().first();
+                            Element e4 = e3.children().first();
+                            eb.setTime(e4.html());
+                        }
+                    }
+                }
+
+            }
+
+            geoCode(eb);
+             formJson(eb);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(EventFinder.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public String getEventPlaces(String place) {
+
+        String res = "";
+        try {
+            Document placeDoc = Jsoup.connect("https://afisha.yandex.ru" + place).get();
+
+            Elements elems = placeDoc.select("p");
+
+            for (Element e : elems) {
+
+                if (e.parents().get(1).html().contains("<div style")) {
+
+                    if (e.children().size() > 1) {
+                        if (e.child(1).hasAttr("href")) {
+                            res = e.child(1).html() + " Москва";
+
+                        }
+                    } else if (e.children().isEmpty()) {
+                        res = e.html() + " Москва";
+                    }
+                }
+
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(EventFinder.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return res;
+    }
+
+    public void formJson(EventBean eb) {
+
+        File fil = new File("/home/alex/NetBeansProjects/JavaLabs/jsons/" + eb.getEventID() + ".json");
+        try {
+            FileOutputStream fos = new FileOutputStream(fil);
+            ObjectMapper om = new ObjectMapper();
+            om.writeValue(fos, eb);
+
+        } catch (IOException ex) {
+            Logger.getLogger(EventFinder.class.getName()).log(Level.SEVERE, null, ex);
+
+        } 
+    
+    
+}
+
+      public void geoCode(EventBean eb) {
+        try{
+          String addr = eb.getPlace().replace(" ", "%20"); 
+            
+          URL url = new URL("http://maps.googleapis.com/maps/api/geocode/json?address="+addr);
+          URLConnection yc = url.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+                String inputLine;
+                StringBuilder sb = new StringBuilder();
+                while ((inputLine = in.readLine()) != null){
+                   
+                    sb.append(inputLine);
+                   
+                }
+   
+          eb.setGeoPos(sb.toString());
+      
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/abondar/javaLabs/SearchData.java
@@ -0,0 +1,153 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.abondar.javaLabs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.lucene.analysis.Analyzer;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.LongField;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.index.IndexableField;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.TopScoreDocCollector;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+import org.codehaus.jettison.json.JSONArray;
+
+/**
+ *
+ * @author alex
+ */
+public class SearchData {
+
+    private final String indexPath = "/home/alex/NetBeansProjects/JavaLabs/jsons/";
+    private final Boolean create = true;
+
+    public void IndexFiles() {
+        try {
+            Directory dir = FSDirectory.open(new File(indexPath));
+                   // FSDirectory.open(new File(indexPath));
+
+            Analyzer an = new StandardAnalyzer(Version.LUCENE_44);
+            IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_44, an);
+
+            if (create) {
+                iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+            } else {
+                iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
+            }
+
+            IndexWriter writer = new IndexWriter(dir, iwc);
+            Path docs = Paths.get(indexPath);
+            indexDocs(writer, docs);
+            writer.close();
+        } catch (IOException ex) {
+            Logger.getLogger(SearchData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void indexDocs(final IndexWriter iw, Path path) throws IOException {
+
+        if (Files.isDirectory(path)) {
+            Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    try {
+                        indexDoc(iw, file, attrs.lastModifiedTime().toMillis());
+                    } catch (IOException ignore) {
+                        // don't index files that can't be read.
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+
+            });
+        } else {
+
+            indexDoc(iw, path, Files.getLastModifiedTime(path).toMillis());
+        }
+    }
+
+    public void indexDoc(IndexWriter iw, Path file, long lastModified) throws IOException {
+        try (InputStream stream = Files.newInputStream(file)) {
+
+            Document doc = new Document();
+            Field pathField = new StringField("path", file.toString(), Field.Store.YES);
+            doc.add(pathField);
+            ObjectMapper mapper = new ObjectMapper();
+            EventBean eb = mapper.readValue(new File(file.toString()), EventBean.class);
+            doc.add(new TextField("category", eb.getCategory(), Field.Store.YES));
+
+            if (iw.getConfig().getOpenMode() == OpenMode.CREATE) {
+                iw.addDocument(doc);
+                for (IndexableField ifd : doc.getFields()) {
+                    System.out.println(ifd.stringValue() + "  " + ifd.name());
+                }
+                System.out.println("adding " + file);
+
+            } else {
+
+                iw.updateDocument(new Term("path", file.toString()), doc);
+                System.out.println("updating " + file);
+            }
+
+        }
+    }
+
+    public String[] getEventData(String evCategory) throws IOException, org.apache.lucene.queryparser.classic.ParseException {
+
+        Analyzer an = new StandardAnalyzer(Version.LUCENE_44);
+
+        Query q = new QueryParser(Version.LUCENE_44, "category", an).parse(evCategory);
+        Directory dir = FSDirectory.open(new File(indexPath));
+
+        IndexReader reader = DirectoryReader.open(dir);
+        Integer numDocs = reader.numDocs();
+      
+        
+        IndexSearcher searcher = new IndexSearcher(reader);
+        ScoreDoc[] hits = searcher.search(q, numDocs).scoreDocs;
+        String[] res = new String[hits.length];
+     
+        for (int i = 0; i < hits.length; i++) {
+            int docId = hits[i].doc;
+
+            res[i]=searcher.doc(docId).get("path");
+         }
+        return res;
+    }
+
+}
